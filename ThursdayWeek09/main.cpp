239682ef6cdb8268abc5e7180a559dc9b34a8703
DwarfKhan <skmiscool@gmail.com>
2018-03-10 16:47:26 -0800

    word reverse exercise

--- ThursdayWeek09/main.cpp
+++ ThursdayWeek09/main.cpp
@@ -5,35 +5,74 @@
 
 using namespace std;
 
-int CharCount(const char* input) {
-	int messageLength = 0;
-	bool messageEnded = false;
+void Reverse(char *begin, char *end);
 
-	while (!messageEnded) {
-		if (input[messageLength] == '\0') {
-			messageEnded = true;
+void reverseWords(char *s)
+{
+	char *word_begin = s;
+	char *temp = s; /* temp is for word boundry */
+
+					/*STEP 1 of the above algorithm */
+	while (*temp)
+	{
+		temp++;
+		if (*temp == '\0')
+		{
+			Reverse(word_begin, temp - 1);
 		}
+		else if (*temp == ' ')
+		{
+			Reverse(word_begin, temp - 1);
+			word_begin = temp + 1;
+		}
+	} /* End of while */
 
-		messageLength++;
-	}
-
-	messageLength -= 1;
-	return messageLength;
+	  /*STEP 2 of the above algorithm */
+	Reverse(s, temp - 1);
 }
 
-int CountWords(const char* input) {
-	int length = CharCount(input);
-	int wordCount = 0;
-	for (int i = 0; i < length; ++i) {
-		if (input[i] == ' ') {
-			wordCount++;
-		}
+//
+//
+//int CharCount(const char* input) {
+//	int messageLength = 0;
+//	bool messageEnded = false;
+//
+//	while (!messageEnded) {
+//		if (input[messageLength] == '\0') {
+//			messageEnded = true;
+//		}
+//
+//		messageLength++;
+//	}
+//
+//	messageLength -= 1;
+//	return messageLength;
+//}
+//
+//int CountWords(const char* input) {
+//	int length = CharCount(input);
+//	int wordCount = 0;
+//	for (int i = 0; i < length; ++i) {
+//		if (input[i] == ' ') {
+//			wordCount++;
+//		}
+//	}
+//	return wordCount;
+//}
+//
+void Reverse(char *begin, char *end)
+{
+	char temp;
+	while (begin < end) 
+	{
+		temp = *begin; //temp is set to first character in range
+		*begin++ = *end;/* the value in the begin position is changed to the value from the end position
+						, then the begin position is incrimented down*/
+		*end-- = temp; //vice-versa
 	}
-	return wordCount;
 }
 
-const char* WordReverse(const char* input) {
-	int wordCount = CountWords(input);
+string WordReverse(const char* input) {
 	char * word_begin = (char*)input; // keeps track of the beginning of the word you are currently reversing
 	char * temp = (char*)input; // keeps track of the end of the word
 
@@ -44,7 +83,7 @@ const char* WordReverse(const char* input) {
 				//if this happens, (bool)*temp will resolve to false
 			}
 			else if (*temp == ' ') { //if we hit a space
-				reverse(word_begin, temp - 1);//reverse everything from word beginning (or beginning of series on fist loop) to temp
+				Reverse(word_begin, temp - 1);//reverse everything from word beginning (or beginning of series on fist loop) to temp
 				word_begin = temp + 1;// move word_begin to the position after the space you just found
 			}
 			
@@ -52,51 +91,39 @@ const char* WordReverse(const char* input) {
 
 	return input;
 }
+//
 
 
-void Reverse(char *begin, char *end)
-{
-	char temp;
-	while (begin < end) 
-	{
-		temp = *begin;
-		*begin++ = *end;// ask question about this
-		*end-- = temp;
-	}
-}
+//void DemonstrateCharCounter() {
+//	cout << "This is a character counter. Please enter some text." << endl;
+//	string input;
+//	getline(cin, input);
+//
+//	char * message = input.c_str;
+//
+//	cout << "This entry has " << CharCount(message) << " characters." << endl;
+//	//cout << "It has " << CountWords(message) << " words." << endl;
+//
+//	std::system("pause");
+//
+//}
 
-void DemonstrateCharCounter() {
-	cout << "This is a character counter. Please enter some text." << endl;
-	string input;
-	cin >> input;
-
-	const char* message = input.c_str;
-
-	cout << "This entry has " << CharCount(message) << " characters." << endl;
-	//cout << "It has " << CountWords(message) << " words." << endl;
-
-	std::system("pause");
-
-}
 
 
 void DemonstrateWordReverse() {
-	cout << "This will reverse words. Please enter some text." << endl;
+	cout << "This will reverse the words' spelling while keeping them in order. Please enter some text." << endl;
 	string input;
-	cin >> input;
-
-	const char* message = input.c_str();
-
-	cout << CharCount(message) << endl;
-
-
-	std::system("pause");
-
+	getline(cin, input);
+	
+	string output = WordReverse(input.c_str());
+	cout << output << endl;
 }
 
 
 int main() {
-	DemonstrateCharCounter();
+
+	//DemonstrateCharCounter();
 	DemonstrateWordReverse();
+	std::system("pause");
 	return 0;
 }
